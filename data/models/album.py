458c01913107ff621c42b71a66f54d8f66f01a23
Marc Meszaros <marc@snapable.com>
2012-07-18 17:17:14 -0400

    Added an optional foreign key to the Album model to server as a cover photo

--- data/models/album.py
+++ data/models/album.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from data.models import Event
+from data.models import Photo
 from data.models import Type
 
 class Album(models.Model):
@@ -11,6 +12,7 @@ class Album(models.Model):
 
     event = models.ForeignKey(Event)
     type = models.ForeignKey(Type)
+    photo = models.ForeignKey(Photo, null=True, default=None, on_delete=models.SET_NULL)
     short_name = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     description = models.TextField()
