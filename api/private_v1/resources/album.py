a6b8585306f279233aac978751e906d13cfd7a13
Marc Meszaros <marc@snapable.com>
2012-07-19 16:29:36 -0400

    Fixed a typo and removed a field from the user resource

--- api/private_v1/resources/album.py
+++ api/private_v1/resources/album.py
@@ -3,7 +3,7 @@ from tastypie.authorization import Authorization
 
 class AlbumResource(api.v1.resources.AlbumResource):
 
-    Meta = api.v1.resources.UserResource.Meta # set Meta to the public API Meta
+    Meta = api.v1.resources.AlbumResource.Meta # set Meta to the public API Meta
     Meta.list_allowed_methods += ['post']
     Meta.authorization = Authorization()
 
--- api/private_v1/resources/user.py
+++ api/private_v1/resources/user.py
@@ -4,7 +4,7 @@ from tastypie.authorization import Authorization
 class UserResource(api.v1.resources.UserResource):
 
     Meta = api.v1.resources.UserResource.Meta # set Meta to the public API Meta
-    Meta.fields += ['password', 'billing_zip', 'terms', 'id']
+    Meta.fields += ['password', 'billing_zip', 'terms']
     Meta.list_allowed_methods += ['post']
     Meta.authorization = Authorization()
 
