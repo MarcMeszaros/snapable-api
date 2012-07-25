829723827124e59c3dbc11fbac309b870c91c5fa
Marc Meszaros <marc@snapable.com>
2012-07-25 15:57:48 -0400

    Added a new URL config to handle the root/base API path

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponseRedirect
 
 import api.v1
 import api.private_v1
@@ -7,9 +8,10 @@ api_v1 = api.v1.SnapableApi()
 private_v1 = api.private_v1.SnapableApi()
 
 urlpatterns = patterns('',
-    # define all the API versions here
-    # the latest API version should work without the number as well
+    # redirect the root API to prevent error pages
+    url(r'^$', lambda x: HttpResponseRedirect('http://snapable.com/')),
 
+    ## define all the API versions here ##
     # public APIs
     url(r'', include(api_v1.urls)),
 
