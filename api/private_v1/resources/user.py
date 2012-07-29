306c92fe3f100a9c7d275e9a3735d09a77475c69
Marc Meszaros <marc@snapable.com>
2012-07-29 15:16:47 -0400

    Added an new auth endpoint to user that only returns data if the correct 'x-snap-user' header is set

--- api/private_v1/resources/user.py
+++ api/private_v1/resources/user.py
@@ -1,7 +1,14 @@
 import api.auth
 import api.v1.resources
+
+from django.conf.urls import url
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
 from tastypie.authorization import Authorization
 from tastypie.exceptions import BadRequest
+from tastypie import http
+
+from data.models import User
 
 class UserResource(api.v1.resources.UserResource):
 
@@ -57,3 +64,59 @@ class UserResource(api.v1.resources.UserResource):
                 raise BadRequest('Missing field: ' + str(key))
 
         return bundle
+
+    # should use prepend_url, but doesn't work...
+    # seems related to this bug: https://github.com/toastdriven/django-tastypie/issues/584
+    def override_urls(self):
+        """
+        Using override_url
+        """
+        return [
+            url(r'^(?P<resource_name>%s)/auth/$' % self._meta.resource_name, self.wrap_view('dispatch_auth'), name="api_dispatch_auth"),
+        ]
+
+    def dispatch_auth(self, request, **kwargs):
+        """
+        A view for handling the various HTTP methods (GET/POST/PUT/DELETE) on
+        a single resource.
+
+        Relies on ``Resource.dispatch`` for the heavy-lifting.
+        """
+        try:
+            # get the header data
+            x_snap_user = request.META['HTTP_X_SNAP_USER']
+            user_details = x_snap_user.strip().split(':')
+
+            # put the email in the kwargs for the dispatch call at the end
+            kwargs['email'] = user_details[0]
+
+            # get the user model matching the email
+            user = User.objects.get(email=user_details[0])
+
+            # get the matched user's password data
+            db_pass = user.password.split('$', 1)
+            pass_data = {}
+
+            # various data based on db_pass type
+            if db_pass[0] == 'bcrypt':
+                pass_data['password_algorithm'] = db_pass[0]
+                pass_data['password_data'] = db_pass[1]
+
+            elif db_pass[0] == 'pbkdf2_sha256':
+                pass_parts = db_pass[1].split('$')
+
+                pass_data['password_algorithm'] = db_pass[0]
+                pass_data['password_iterations'] = pass_parts[0]
+                pass_data['password_salt'] = pass_parts[1]
+                pass_data['password_hash'] = pass_parts[2]
+
+            # if the db password hash and the one in the header match, display the user details
+            if pass_data['password_hash'] == user_details[1]:
+                return self.dispatch('detail', request, **kwargs)
+            else:
+                raise BadRequest('Invalid email/password hash combination in header X-Snap-User')
+
+        except KeyError as key:
+            raise BadRequest('Missing field: ' + str(key))
+        except ObjectDoesNotExist:
+            raise BadRequest('Invalid email/password hash combination in header X-Snap-User')
