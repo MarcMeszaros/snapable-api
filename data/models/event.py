3b8c2b24f9d131c80b5c9883aa388e1952aebd12
Marc Meszaros <marc@snapable.com>
2012-08-23 11:36:59 -0400

    Fixed a typo

--- data/models/event.py
+++ data/models/event.py
@@ -20,7 +20,7 @@ class Event(models.Model):
     end = models.DateTimeField(help_text='Event end time. (UTC)')
     title = models.CharField(max_length=255, help_text='Event title.')
     url = models.CharField(max_length=255, help_text='A "short name" for the event.')
-    pin = models.CharField(max_length=255, help_text='Pseudoramdom PIN used for private events.')
+    pin = models.CharField(max_length=255, help_text='Pseudo-random PIN used for private events.')
     creation_date = models.DateTimeField(auto_now_add=True, help_text='When the event created. (UTC)')
     last_access = models.DateTimeField(auto_now_add=True, help_text='When the event was last accessed. (UTC)')
     access_count = models.IntegerField(default=0)
