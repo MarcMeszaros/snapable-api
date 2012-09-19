ba83c11d9cf19a9fbd97e91dd5dd20f647a25144
Marc Meszaros <marc@snapable.com>
2012-09-19 10:15:47 -0400

    Removed non-used settings

--- api/settings.py
+++ api/settings.py
@@ -101,17 +101,17 @@ SECRET_KEY = '***REMOVED***'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
+    #'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    #'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    #'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -134,6 +134,9 @@ INSTALLED_APPS = (
     #'django.contrib.sites',
     #'django.contrib.messages',
     #'django.contrib.staticfiles',
+
+    # core apps for snapable
+    'api',
 
     # various sub-apps for snapable
     'data',
