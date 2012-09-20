3560b7c6088820e1477cadd9ed1eb452e415d066
Marc Meszaros <marc@snapable.com>
2012-09-20 12:31:26 -0400

    Integrate the NewRelic client as part of the API for monitoring

--- manage.py
+++ manage.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 import os
+import socket
 import sys
 
 if __name__ == "__main__":
     sys.path.append('../')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+
+    # start newrelic if on athena (staging)
+    if ('athena' in socket.gethostname()):
+        import newrelic.agent
+        newrelic.agent.initialize('newrelic.ini', 'staging')
+    # start newrelic if on ares (production)
+    elif ('ares' in socket.gethostname()):
+        import newrelic.agent
+        newrelic.agent.initialize('newrelic.ini', 'production')
 
     from django.core.management import execute_from_command_line
 
