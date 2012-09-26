84c26342b734d826b6fc29cab5724a3e70b5bf35
Marc Meszaros <marc@snapable.com>
2012-09-26 12:19:17 -0400

    Fixes a small typo

--- data/models/addon.py
+++ data/models/addon.py
@@ -8,5 +8,5 @@ class Addon(models.Model):
 
     title = models.CharField(max_length=255, help_text='The title of the addon.')
     description = models.TextField(help_text='The addon description.')
-    price = models.DecimalField(max_digits=6, decimal_places=2, help_text='The per unit adddon price.') # 9999.99
+    price = models.DecimalField(max_digits=6, decimal_places=2, help_text='The per unit addon price.') # 9999.99
     enabled = models.BooleanField(default=True, help_text='If the addon is enabled or not.')
