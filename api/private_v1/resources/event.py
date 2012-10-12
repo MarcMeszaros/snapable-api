d06b1b79e061cc38fb854f6d8cabb5a28c41d893
Marc Meszaros <marc@snapable.com>
2012-10-11 22:43:19 -0400

    Events resource limits result by time if lat/lng variables are put as parameters

--- api/private_v1/resources/event.py
+++ api/private_v1/resources/event.py
@@ -1,10 +1,13 @@
 import api.auth
 import api.v1.resources
 import copy
+import pytz
 
+from datetime import datetime, timedelta
 from decimal import Decimal
 
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.db.models import Q
 from django.http import HttpResponse
 
 from tastypie import fields
@@ -78,12 +81,17 @@ class EventResource(api.v1.resources.EventResource):
 
             # 0.000001 = 0.111 m
             # 0.000001 * 450450 = 0.450449 ~ 50km
+            # 0.000001 * 225255 = 0.225225 ~ 25km
+            # 0.000001 * 90090 = 0.09009 ~ 10km
+            # 0.000001 * 45045 = 0.045045 ~ 5km
+            # 0.000001 * 18018 = 0.018018 ~ 2km
+            # 0.000001 * 9009 = 0.009009 ~ 1km
             # variance calculation
-            delta = Decimal('0.450449')
-            lat_lower = Decimal(request.GET['lat']) - delta
-            lat_upper = Decimal(request.GET['lat']) + delta
-            lng_lower = Decimal(request.GET['lng']) - delta
-            lng_upper = Decimal(request.GET['lng']) + delta
+            delta_distance = Decimal('0.450449')
+            lat_lower = Decimal(request.GET['lat']) - delta_distance
+            lat_upper = Decimal(request.GET['lat']) + delta_distance
+            lng_lower = Decimal(request.GET['lng']) - delta_distance
+            lng_upper = Decimal(request.GET['lng']) + delta_distance
 
             # get addresses
             addresses = Address.objects.filter(lat__gte=lat_lower, lat__lte=lat_upper, lng__gte=lng_lower, lng__lte=lng_upper)
@@ -92,8 +100,47 @@ class EventResource(api.v1.resources.EventResource):
             for value in values:
                 values_list.append(value['event_id'])
 
-            # get events and return
+            # get base events
             events = super(EventResource, self).get_object_list(request)
+
+            # the possible time GET params to filter on events
+            possible_time_params = [
+                'start', 'start__gt', 'start__gte', 'start__lt', 'start__lte',
+                'end', 'end__gt', 'end__gte', 'end__lt', 'end__lte',
+            ]
+
+            # fancy algorithm to detect if there are none of the time filtering GET params
+            # if there is not time filtering, apply our default time filtering
+            #
+            # the condition check works as follows:
+            # 1. get the request param keys and turn it into a set, turn the possible params into a set
+            # 2. do the intersect between both sets ('&' operator)
+            # 3. convert the resulting interset into a list
+            # 4. check the list length to see how many possible time filters are in the request (if 0, do our default algorithm)
+            if (len(list(set(request.GET.keys()) & set(possible_time_params))) == 0):
+
+                # query filtering variables
+                delta_minutes = 48 * 60
+                now_datetime = datetime.now(pytz.utc) # current time on server
+                pre_now_datetime = now_datetime + timedelta(0, 0, 0, 0, -delta_minutes) # delta minutes in the past
+                post_now_datetime = now_datetime + timedelta(0, 0, 0, 0, delta_minutes) # delta minutes in the future
+
+                # default queryset filtering for events to only show events +/- delta or in progress:
+                #
+                #      start    end
+                # -------|-------|-------> time
+                #    ^       ^       ^
+                #    1       2       3     possible points during an event
+                #
+                # delta = 24 hours
+                # 1 = (start > now && start <= (now + delta))
+                # 2 = (start < now && end > now)
+                # 3 = (end < now && end >= (now - delta))
+                #
+                events = events.filter(
+                    (Q(start__gt=now_datetime) & Q(start__lte=post_now_datetime)) | (Q(start__lte=now_datetime) & Q(end__gte=now_datetime)) | (Q(end__lt=now_datetime) & Q(end__gte=pre_now_datetime))
+                )
+
             return events.filter(pk__in=values_list)
         else:
             return super(EventResource, self).get_object_list(request)
