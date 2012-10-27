8e8826f4a8a7a7c07ec26cda756e7b8149073e9e
Marc Meszaros <marc@snapable.com>
2012-10-27 00:50:33 -0400

    Added input/ouput checks and formating for order resources

--- api/private_v1/resources/order.py
+++ api/private_v1/resources/order.py
@@ -1,8 +1,17 @@
+import api.auth
+
 from tastypie import fields
 from tastypie.resources import ModelResource
 from data.models import Order
 
+from tastypie.authorization import Authorization
+from tastypie.exceptions import BadRequest
+
 from account import AccountResource
+
+from data.models import AccountAddon
+from data.models import EventAddon
+from data.models import Package
 
 class OrderResource(ModelResource):
 
@@ -14,3 +23,63 @@ class OrderResource(ModelResource):
         list_allowed_methods = ['get', 'post']
         detail_allowed_methods = ['get', 'post', 'put']
         always_return_data = True
+        authentication = api.auth.ServerAuthentication()
+        authorization = Authorization()
+
+    def dehydrate(self, bundle):
+        # small hack required to make the field return as json
+        bundle.data['items'] = bundle.obj.items
+
+        return bundle
+
+
+    def hydrate(self, bundle):
+        # check the items data if it's set
+        if bundle.data.has_key('items'):
+            # get a handle on the items
+            items = bundle.data['items']
+
+            # make sure it is a JSONObject
+            if type(items) is not dict:
+                raise BadRequest('Must be a JSONObject.')
+
+            # make sure the 3 different fields aren't missing
+            if not items.has_key('package'):
+                raise BadRequest('Missing "package" identifier.')
+            if not items.has_key('account_addons') or type(items['account_addons']) is not list:
+                raise BadRequest('Missing/improperly formated "account_addons" list.')
+            if not items.has_key('event_addons') or type(items['event_addons']) is not list:
+                raise BadRequest('Missing/improperly formated "event_addons" list.')
+
+            # verify the data exists
+            if type(items['package']) is int and not Package.objects.filter(pk=items['package']).exists():
+                raise BadRequest('The entered package does not exist.')
+            if type(items['account_addons']) is list and len(items['account_addons']) > 0 and not AccountAddon.objects.filter(pk__in=items['account_addons']).exists():
+                raise BadRequest('One or more account addon identifiers does not exist.')
+            if type(items['event_addons']) is list and len(items['event_addons']) > 0 and not EventAddon.objects.filter(pk__in=items['event_addons']).exists():
+                raise BadRequest('One or more event addon identifiers does not exist.')
+
+        return bundle
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        bundle = super(OrderResource, self).obj_create(bundle, request)
+
+        # get the order
+        order = bundle.obj
+
+        # loop through account_addons & event_addons and mark as paid
+        # mark all the account addons as paid for
+        for account_addon in order.items['account_addons']:
+            addon = AccountAddon.objects.get(pk=account_addon)
+            addon.paid = True
+            addon.order = order
+            addon.save()
+
+        # mark all the event addons as paid for
+        for event_addon in order.items['event_addons']:
+            addon = EventAddon.objects.get(pk=event_addon)
+            addon.paid = True
+            addon.order = order
+            addon.save()
+
+        return bundle
