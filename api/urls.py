b656ba6cdfc8de343d1dc8f8b5d90e2cc577f51c
Marc Meszaros <marc@snapable.com>
2012-11-04 12:45:17 -0500

    Added a 'robots.txt' to urls to stop bots from crawling the API

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 import api.v1
 import api.private_v1
@@ -10,6 +10,8 @@ private_v1 = api.private_v1.SnapableApi()
 urlpatterns = patterns('',
     # redirect the root API to prevent error pages
     url(r'^$', lambda x: HttpResponseRedirect('http://snapable.com/')),
+    # tell bots not to try and crawl the API
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /*", mimetype="text/plain")),
 
     ## define all the API versions here ##
     # public APIs
