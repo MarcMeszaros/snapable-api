31ea9695ddc61814f9796d0817a77ab1373cc681
Marc Meszaros <marc@snapable.com>
2012-11-22 16:20:14 -0500

    Moved some of the backend code to save images to Cloud Files into the model

--- api/serializers.py
+++ api/serializers.py
@@ -30,37 +30,8 @@ class PhotoSerializer(Serializer):
         if (bundle.data != None and bundle.data.has_key('size')):
             size = bundle.data['size']
 
-        #connect to container
-        try:
-            conn = cloudfiles.Connection(settings.RACKSPACE_USERNAME, settings.RACKSPACE_APIKEY, settings.RACKSPACE_CLOUDFILE_TIMEOUT)
-            cont = conn.get_container(settings.RACKSPACE_CLOUDFILE_CONTAINER_PREFIX + str(event.id / 1000))
-
-            # try an get the size wanted
-            try:
-                obj = cont.get_object(str(event.id) + '/' + str(photo.id) + '_' + size + '.jpg')
-                return obj.read()
-            except:
-                # get the original size photo and create a Image object based on it
-                obj_orig = cont.get_object(str(event.id) + '/' + str(photo.id) + '_orig.jpg')
-                data = obj_orig.read()
-                img = Image.open(StringIO.StringIO(data))
-                snapimg = SnapImage(img)
-                
-                # get the size param and resize
-                sizeList = size.split('x')
-                sizeTupple = (int(sizeList[0]), int(sizeList[1]))
-                snapimg.resize(sizeTupple)
-
-                # save the new photo size
-                obj = cont.create_object(str(event.id) + '/' + str(photo.id) + '_' + size + '.jpg')
-                obj.content_type = 'image/jpeg'
-                obj.write(snapimg.img.tostring('jpeg', 'RGB'))
-
-                return snapimg.img.tostring('jpeg', 'RGB')
-        except cloudfiles.errors.NoSuchObject as e:
-            raise tastypie.exceptions.ImmediateHttpResponse(tastypie.http.HttpNotFound())
-        except cloudfiles.errors.NoSuchContainer as e:
-            raise tastypie.exceptions.ImmediateHttpResponse(tastypie.http.HttpNotFound())
+            snapimg = photo.getImage(size)
+            return snapimg.img.tostring('jpeg', 'RGB')
 
     def from_jpeg(self, content):
         data = []
--- data/models/photo.py
+++ data/models/photo.py
@@ -1,5 +1,11 @@
+import cloudfiles
+import StringIO
+from PIL import Image
+
+from django.conf import settings
 from django.db import models
 
+from data.images import SnapImage
 from data.models import Event
 from data.models import Guest
 from data.models import Type
@@ -19,3 +25,60 @@ class Photo(models.Model):
     streamable = models.BooleanField(help_text='If the photo is streamable.')
     timestamp = models.DateTimeField(auto_now_add=True, help_text='The photo timestamp.')
     metrics = models.TextField(help_text='JSON metrics about the photo.') # JSON metrics
+
+    # helper functions for the image storage
+    def getImage(self, size='orig'):
+        """
+        Get the SnapImage from Cloud Files.
+        """
+        if self.id != None and self.event != None:
+            #connect to container
+            try:
+                conn = cloudfiles.Connection(settings.RACKSPACE_USERNAME, settings.RACKSPACE_APIKEY, settings.RACKSPACE_CLOUDFILE_TIMEOUT)
+                cont = conn.get_container(settings.RACKSPACE_CLOUDFILE_CONTAINER_PREFIX + str(self.event.id / 1000))
+
+                # try an get the size wanted
+                try:
+                    obj = cont.get_object(str(self.event.id) + '/' + str(self.id) + '_' + size + '.jpg')
+                    img = Image.open(StringIO.StringIO(obj.read()))
+                    snapimg = SnapImage(img)
+
+                    return snapimg
+                except:
+                    obj = cont.get_object(str(self.event.id) + '/' + str(self.id) + '_orig.jpg')
+                    img = Image.open(StringIO.StringIO(obj.read()))
+                    snapimg = SnapImage(img)
+
+                    # resize the image
+                    sizeList = size.split('x')
+                    sizeTupple = (int(sizeList[0]), int(sizeList[1]))
+                    snapimg.resize(sizeTupple)
+
+                    # save the new photo size
+                    obj = cont.create_object(str(self.event.id) + '/' + str(self.id) + '_' + size + '.jpg')
+                    obj.content_type = 'image/jpeg'
+                    obj.write(snapimg.img.tostring('jpeg', 'RGB'))
+
+                    return snapimg
+
+            except cloudfiles.errors.NoSuchObject as e:
+                return None
+            except cloudfiles.errors.NoSuchContainer as e:
+                return None
+
+        else:
+            raise Exception('No Photo ID and/or Event ForeignKey specified.')
+
+    def saveImage(self, image, orig=False):
+        """
+        Save the SnapImage to CloudFiles.
+        """
+        if isinstance(image, SnapImage):
+            width, height = image.img.size
+
+            # save the new photo size
+            #obj = cont.create_object(str(self.event.id) + '/' + str(self.id) + '_' + size + '.jpg')
+            #obj.content_type = 'image/jpeg'
+            #obj.write(snapimg.img.tostring('jpeg', 'RGB'))
+        else:
+            raise TypeError
