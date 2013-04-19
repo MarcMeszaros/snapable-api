2c865425f3aa5075d4de47b173d694672bf348e4
Marc Meszaros <marc@snapable.com>
2013-04-19 16:28:52 -0400

    Add unittests for photo uploading on the private API

--- api/private_v1/tests/photo.py
+++ api/private_v1/tests/photo.py
@@ -1,29 +1,66 @@
 # python
-import datetime
+import os
+import re
 
 # django/tastypie
-from django.contrib.auth.models import User
-from tastypie.test import ResourceTestCase
+from tastypie.test import ResourceTestCase, TestApiClient
+from PIL import Image
 
 # snapable
 from api.auth.server import ServerAuthentication
+from api.utils.serializers import MultipartSerializer
 
 class PhotoResourceTest(ResourceTestCase):
-    
+    fixtures = ['packages.json', 'accounts_and_users.json', 'events.json', 'photos.json']
+
     def setUp(self):
         super(PhotoResourceTest, self).setUp()
-        self.api_key = 'abc123'
-        self.api_secret = 'abc'
+        # we need a custom serializer for multipart uploads
+        self.api_client = TestApiClient(serializer=MultipartSerializer())
+        self.api_key = 'key123'
+        self.api_secret = 'sec123'
+
+        # The data we'll send on POST requests.
+        filename = 'trashcat.jpg'
+        filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
+        f = open(filepath, 'rb')
+
+        self.post_data = {
+            'event': '/private_v1/event/1/',
+            'caption': 'My super awesome caption!',
+            'type': '/private_v1/type/1/',
+            'image': {
+                'filename': filename,
+                'data': f.read(),
+            }
+        }
 
     def get_credentials(self, method, uri):
         return ServerAuthentication.create_signature(self.api_key, self.api_secret, method, uri)
 
-    def test_unauthorized(self):
-        method = 'GET'
-        uri = '/private_v1/event/'
-        self.assertHttpUnauthorized(self.api_client.get(uri, format='json', authentication=ServerAuthentication.create_signature('invalidkey', self.api_secret, method, uri)))
-        self.assertHttpUnauthorized(self.api_client.get(uri, format='json', authentication=ServerAuthentication.create_signature(self.api_key, 'invalidsecret', method, uri)))
+    def test_post_photo(self):
+        uri = '/private_v1/photo/'
+        resp = self.api_client.post(uri, data=self.post_data, format='multipart', authentication=self.get_credentials('POST', uri))
+        data = self.deserialize(resp)
 
-    def test_create_photo_timestamp(self):
-        # not implemented yet
-        pass
+        # make sure the resource was created
+        self.assertHttpCreated(resp)
+        
+        # test to make sure all the keys are in the response
+        self.assertKeys(self.deserialize(resp), [
+            'author_name',
+            'caption',
+            'event',
+            'guest',
+            'image',
+            'metrics',
+            'resource_uri',
+            'streamable',
+            'timestamp',
+            'type',
+        ])
+
+        # make sure the timestamp is in the format we want (ISO8601) without micro
+        # expected format: 2013-01-21T18:12:45
+        self.assertTrue(re.search('^\d{4}-(\d{2}-?){2}T(\d{2}:?){3}$', data['timestamp']))
+        
