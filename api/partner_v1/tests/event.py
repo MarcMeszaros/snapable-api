fc43fb94e2e0955b9ad262902bb489442865d0ce
Marc Meszaros <marc@snapable.com>
2013-05-02 11:16:51 -0400

    Added event tests to the partner API

--- api/partner_v1/tests/event.py
+++ api/partner_v1/tests/event.py
@@ -4,35 +4,35 @@ from tastypie.test import ResourceTestCase
 # snapable
 from api.auth.db_v1 import DatabaseAuthentication
 from api.models import ApiAccount
-from data.models import Account
+from data.models import Event
 
-class Partner_v1__AccountResourceTest(ResourceTestCase):
-    fixtures = ['api_accounts_and_keys.json', 'packages.json', 'accounts_and_users.json']
+class Partner_v1__EventResourceTest(ResourceTestCase):
+    fixtures = ['api_accounts_and_keys.json', 'packages.json', 'accounts_and_users.json', 'events.json']
 
     def setUp(self):
-        super(Partner_v1__AccountResourceTest, self).setUp()
+        super(Partner_v1__EventResourceTest, self).setUp()
         self.api_key = 'key123'
         self.api_secret = 'sec123'
 
         self.api_account_1 = ApiAccount.objects.get()
-        self.accounts = Account.objects.filter(api_account=self.api_account_1)
-        self.account_1 = self.accounts[0]
+        self.events = Event.objects.filter(account__api_account=self.api_account_1)
+        self.event_1 = self.events[0]
 
     def get_credentials(self, method, uri):
         return DatabaseAuthentication.create_signature(self.api_key, self.api_secret, method, uri)
 
-    def test_get_accounts(self):
-        uri = '/partner_v1/account/'
+    def test_get_events(self):
+        uri = '/partner_v1/event/'
         resp = self.api_client.get(uri, format='json', authentication=self.get_credentials('GET', uri))
 
         # make sure the resource is valid
         self.assertValidJSONResponse(resp)
 
         # make sure we have the right number of accounts
-        self.assertEqual(len(self.deserialize(resp)['objects']), self.accounts.count())
+        self.assertEqual(len(self.deserialize(resp)['objects']), self.events.count())
 
-    def test_get_account(self):
-        uri = '/partner_v1/account/{0}/'.format(self.account_1.pk)
+    def test_get_event(self):
+        uri = '/partner_v1/event/{0}/'.format(self.event_1.pk)
         resp = self.api_client.get(uri, format='json', authentication=self.get_credentials('GET', uri))
 
         # make sure the resource is valid
@@ -40,6 +40,17 @@ class Partner_v1__AccountResourceTest(ResourceTestCase):
 
         # test to make sure all the keys are in the response
         self.assertKeys(self.deserialize(resp), [
+            'account',
+            'created',
+            'enabled',
+            'end',
+            'locations',
+            'photo_count',
+            'pin',
+            'public',
             'resource_uri',
-            'users',
+            'start',
+            'title',
+            'tz_offset',
+            'url',
         ])
