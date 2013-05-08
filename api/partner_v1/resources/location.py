6580357e6fec92308e3b77c54070541d28ea431d
Marc Meszaros <marc@snapable.com>
2013-05-08 14:31:15 -0400

    Make sure only one location can be added to an event

--- api/partner_v1/resources/location.py
+++ api/partner_v1/resources/location.py
@@ -1,12 +1,34 @@
 # django/tastypie/libs
 from tastypie import fields
 from tastypie.authorization import Authorization
+from tastypie.validation import Validation
 
 # snapable
 import api.auth
 import api.base_v1.resources
 
+from data.models import Event
 from event import EventResource
+
+class LocationValidation(Validation):
+    def is_valid(self, bundle, request=None):
+        errors = {}
+
+        #required = ['address', 'lat', 'lng']
+        #for key in required:
+        #    try:
+        #        bundle.data[key]
+        #    except KeyError:
+        #        errors[key] = 'Missing field'
+
+        # make sure there's only one account
+        event_str = bundle.data['event']
+        event_parts = event_str.strip('/').split('/')
+        event = Event.objects.get(pk=event_parts[-1])
+        if event.address_set.count() >= 1:
+            errors['event'] = 'Only one location per event allowed with the partner API.'
+
+        return errors
 
 class LocationResource(api.base_v1.resources.AddressResource):
 
@@ -18,6 +40,7 @@ class LocationResource(api.base_v1.resources.AddressResource):
         detail_allowed_methods = ['get', 'post', 'put', 'delete']
         authentication = api.auth.DatabaseAuthentication()
         authorization = api.auth.DatabaseAuthorization()
+        validation = LocationValidation()
         filtering = {
             'event': ['exact'],
         }
--- api/partner_v1/tests/location.py
+++ api/partner_v1/tests/location.py
@@ -4,7 +4,7 @@ from tastypie.test import ResourceTestCase
 # snapable
 from api.auth.db_v1 import DatabaseAuthentication
 from api.models import ApiAccount
-from data.models import Address
+from data.models import Address, Event
 
 class Partner_v1__LocationResourceTest(ResourceTestCase):
     fixtures = ['api_accounts_and_keys.json', 'packages.json', 'accounts_and_users.json', 'events.json', 'guests.json']
@@ -15,8 +15,16 @@ class Partner_v1__LocationResourceTest(ResourceTestCase):
         self.api_secret = 'sec123'
 
         self.api_account_1 = ApiAccount.objects.all()[0]
+        self.events = Event.objects.filter(account__api_account=self.api_account_1)
         self.locations = Address.objects.filter(event__account__api_account=self.api_account_1)
         self.location_1 = self.locations[0]
+
+        self.post_data = {
+            'event': '/partner_v1/event/{0}/'.format(self.events[0].pk),
+            'address': '123 My Address St., SomeTown, Province, Country',
+            'lat': '123.000000',
+            'lng': '90.000000',
+        }
 
     def get_credentials(self, method, uri):
         return DatabaseAuthentication.create_signature(self.api_key, self.api_secret, method, uri)
@@ -46,3 +54,12 @@ class Partner_v1__LocationResourceTest(ResourceTestCase):
             'lng',
             'resource_uri',
         ])
+
+    def test_post_location(self):
+        uri = '/partner_v1/location/'
+        self.assertEqual(self.events[0].address_set.count(), 1)
+        resp = self.api_client.post(uri, data=self.post_data, format='json', authentication=self.get_credentials('POST', uri))
+
+        # make sure the we can't add more than one location
+        self.assertHttpBadRequest(resp)
+        self.assertEqual(self.events[0].address_set.count(), 1)
