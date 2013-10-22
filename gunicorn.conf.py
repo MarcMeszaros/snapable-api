2fd45ba4b4c7c6f58528b623c61a8b23f8d9c1f8
Marc Meszaros <marc@snapable.com>
2013-10-22 02:31:10 -0400

    Have gunicorn create the 'logs' folder and print logs to the console

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,14 @@
 import multiprocessing
 import netifaces
+import os
 import socket
+
+# get the folder path
+PROJECT_PATH = os.path.dirname(__file__)
+
+# create the 'logs' folder(s) if it doesn't already exist
+if not os.path.exists(os.path.join(PROJECT_PATH, 'logs')):
+    os.makedirs(os.path.join(PROJECT_PATH, 'logs'))
 
 # some config variables
 cfg_eth0_address = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr'] # usually the public IP on Rackspace/VM
@@ -16,3 +24,7 @@ bind = [
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'sync' # default: sync; alternate: gevent
 proc_name = 'snap_api'
+
+# logging
+accesslog = '-'
+errorlog = '-'
