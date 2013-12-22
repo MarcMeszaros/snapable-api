6ad7077b74b6ae961b901697a2dd3a1e3df74c68
Marc Meszaros <marc@snapable.com>
2013-12-22 11:32:05 -0500

    Update the requirements

--- script/api_dependencies.sh
+++ script/api_dependencies.sh
@@ -7,4 +7,4 @@ echo "| Install System Packages |"
 echo "+-------------------------+"
 echo ""
 apt-get update
-apt-get -y install ntp git make python-dev python-pip libevent-dev libmysqlclient-dev libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms1-dev libwebp-dev
+apt-get -y install ntp git make python-dev python-pip libffi-dev libmysqlclient-dev libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms1-dev libwebp-dev
--- script/worker_dependencies.sh
+++ script/worker_dependencies.sh
@@ -14,5 +14,5 @@ echo "| Install System Packages |"
 echo "+-------------------------+"
 echo ""
 apt-get update
-apt-get -y install ntp git make python-dev python-pip libevent-dev libmysqlclient-dev libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms1-dev libwebp-dev 
+apt-get -y install ntp git make python-dev python-pip libffi-dev libmysqlclient-dev libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms1-dev libwebp-dev 
 apt-get -y install rabbitmq-server
