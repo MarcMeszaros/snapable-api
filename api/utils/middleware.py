c8286f7257a791bfab2f0e3e7e16ef43c13d2c22
Marc Meszaros <marc@snapable.com>
2013-12-24 15:12:29 -0500

    Failing to log shouldn't cause an API error

--- api/utils/middleware.py
+++ api/utils/middleware.py
@@ -12,15 +12,15 @@ class RequestLoggingMiddleware(object):
         return None
 
     def process_response(self, request, response):
-        duration = (time() - request.timer) * 1000 # milliseconds
+        try:
+            duration = (time() - request.timer) * 1000 # milliseconds
 
-        # log the response
-        logger = logging.getLogger('snapable.request')
+            # log the response
+            logger = logging.getLogger('snapable.request')
 
-        # log the request
-        if 'HTTP_ACCEPT' in request.META:
+            # log the request
             logger.info('{0} {1} [{2}] ({3}) {4}'.format(request.method, response.status_code, duration, request.META['HTTP_ACCEPT'], request.path))
-        else:
-            logger.info('{0} {1} {2}'.format(request.method, response.status_code, request.path))
+        except Exception:
+            pass # if anything goes wrong, just keep going
 
         return response
