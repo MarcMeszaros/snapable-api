b85f476a3a8e2d0269bc4567373fb2d10d0de8ec
JonathanSeguin <jonathan@snapable.com>
2014-01-06 02:29:21 -0500

    WIP fixed fetching and saving all photos in tmp folder

--- worker/event.py
+++ worker/event.py
@@ -6,6 +6,7 @@ from worker import app
 import os
 import shutil
 import zipfile
+import tempfile
 
 from datetime import datetime, timedelta
 
@@ -25,7 +26,7 @@ def create_images_zip(event_id):
 
         # loop through all the photos, and save to disk on the worker server
         for photo in photos:
-            photo.img.save('{0}/{1}.jpg'.format(tempdir, photo.pk))
+            photo.get_image().img.save('{0}/{1}.jpg'.format(tempdir, photo.pk))
 
         # create the zip file [PS: awesome new python 2.7+ function! :-) ]
         zip_path = shutil.make_archive(photo.pk, 'zip', event.url)
@@ -39,7 +40,7 @@ def create_images_zip(event_id):
             cont = conn.create_container(settings.RACKSPACE_CLOUDFILE_DOWNLOAD_CONTAINER_PREFIX + str(event.pk / settings.RACKSPACE_CLOUDFILE_EVENTS_PER_CONTAINER))
             conn.make_container_public(cont.name)
             Log.i('created a new CDN container: ' + settings.RACKSPACE_CLOUDFILE_DOWNLOAD_CONTAINER_PREFIX + str(self.event.pk / settings.RACKSPACE_CLOUDFILE_EVENTS_PER_CONTAINER))
-        
+    
         cont.upload_file(zip_path)
 
         # delete local zip
