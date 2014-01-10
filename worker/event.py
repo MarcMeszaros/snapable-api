f8eba11dd449704965ec6f20fec32f76a56593b9
JonathanSeguin <jonathan@snapable.com>
2014-01-09 22:10:56 -0500

    (Oops) Fix bug in last commit

--- worker/event.py
+++ worker/event.py
@@ -32,72 +32,72 @@ from api.utils.loggers import Log
 
 @app.task
 def create_album_zip(event_id):
+
+    event = Event.objects.get(pk=event_id)
+
+    # establish a connection to the CDN
+    conn = pyrax.connect_to_cloudfiles(public=settings.RACKSPACE_CLOUDFILE_PUBLIC_NETWORK)
+    cont = None
     try:
+        cont = conn.get_container(settings.RACKSPACE_CLOUDFILE_DOWNLOAD_CONTAINER_PREFIX + str(event.pk / settings.RACKSPACE_CLOUDFILE_EVENTS_PER_CONTAINER))
+    except pyrax.exceptions.NoSuchContainer as e:
+        cont = conn.create_container(settings.RACKSPACE_CLOUDFILE_DOWNLOAD_CONTAINER_PREFIX + str(event.pk / settings.RACKSPACE_CLOUDFILE_EVENTS_PER_CONTAINER))
+        conn.make_container_public(cont.name)
+        Log.i('created a new CDN container: ' + settings.RACKSPACE_CLOUDFILE_DOWNLOAD_CONTAINER_PREFIX + str(event.pk / settings.RACKSPACE_CLOUDFILE_EVENTS_PER_CONTAINER))
 
-        event = Event.objects.get(pk=event_id)
 
-        # establish a connection to the CDN
-        conn = pyrax.connect_to_cloudfiles(public=settings.RACKSPACE_CLOUDFILE_PUBLIC_NETWORK)
-        cont = None
-        try:
-            cont = conn.get_container(settings.RACKSPACE_CLOUDFILE_DOWNLOAD_CONTAINER_PREFIX + str(event.pk / settings.RACKSPACE_CLOUDFILE_EVENTS_PER_CONTAINER))
-        except pyrax.exceptions.NoSuchContainer as e:
-            cont = conn.create_container(settings.RACKSPACE_CLOUDFILE_DOWNLOAD_CONTAINER_PREFIX + str(event.pk / settings.RACKSPACE_CLOUDFILE_EVENTS_PER_CONTAINER))
-            conn.make_container_public(cont.name)
-            Log.i('created a new CDN container: ' + settings.RACKSPACE_CLOUDFILE_DOWNLOAD_CONTAINER_PREFIX + str(event.pk / settings.RACKSPACE_CLOUDFILE_EVENTS_PER_CONTAINER))
+    try:
+        # check if zip already created
+        zip_obj = cont.get_object(event.title + ".zip")
 
-        try:
-            # check if zip already created
-            zip_obj = cont.get_object(event.title + ".zip")
+    except pyrax.exceptions.NoSuchObject as e:
+        # no zip for event on CDN yet -> upload to the CDN
+        zip_obj = cont.upload_file(zip_path)
 
-        except pyrax.exceptions.NoSuchObject as e:
-            # no zip for event on CDN yet -> upload to the CDN
+    else:
+        td = datetime.utcnow() - parser.parse(zip_obj.last_modified)
+
+        # zip was created a while ago (>15min) -> recreate and reupload
+        if td.seconds/60 > 15:
+            # create tempdir and get the event
+            tempdir = tempfile.mkdtemp(prefix='snap_api_event_{0}_'.format(event_id))
+            photos = event.photo_set.all()
+
+            # loop through all the photos, and save to disk on the worker server
+            for photo in photos:
+                photo.get_image().img.save('{0}/{1}.jpg'.format(tempdir, photo.pk))
+
+            # create and (re)upload the zip file
+            zip_path = shutil.make_archive(tempfile.tempdir+"/"+event.title,'zip', tempdir)
+            zip_obj.delete
             zip_obj = cont.upload_file(zip_path)
 
-        else:
-            td = datetime.utcnow() - parser.parse(zip_obj.last_modified)
+            # delete temp
+            shutil.rmtree(tempdir)
 
-            # zip was created a while ago (>15min) -> recreate and reupload
-            if td.seconds/60 > 15:
-                # create tempdir and get the event
-                tempdir = tempfile.mkdtemp(prefix='snap_api_event_{0}_'.format(event_id))
-                                photos = event.photo_set.all()
-
-                # loop through all the photos, and save to disk on the worker server
-                for photo in photos:
-                    photo.get_image().img.save('{0}/{1}.jpg'.format(tempdir, photo.pk))
-
-                # create and (re)upload the zip file
-                zip_path = shutil.make_archive(tempfile.tempdir+"/"+event.title,'zip', tempdir)
-                zip_obj.delete
-                zip_obj = cont.upload_file(zip_path)
-
-            # issue temp URL (24h = 86400 seconds)
-            zip_temp_url = zip_obj.get_temp_url(86400)
-
-        finally:
-            # mail zip url
-
-            # load in the templates
-            plaintext = get_template('zip_url.txt')
-            html = get_template('zip_url.html')
-
-            # setup the template context variables
-            d = Context({ 'zip_temp_url': zip_temp_url })
-
-            # build the email
-            email = AccountUser.objects.get(account_id=event.account_id).user.email
-            subject, from_email, to = 'Your Snapable album is ready for download', 'support@snapable.com', [email]
-            text_content = plaintext.render(d)
-            html_content = html.render(d)
-            msg = EmailMultiAlternatives(subject, text_content, from_email, to)
-            msg.attach_alternative(html_content, "text/html")
-            #if settings.DEBUG == False:
-            msg.send()
-
-            # delete local zip
-            os.remove(zip_path)
+        # issue temp URL (24h = 86400 seconds)
+        zip_temp_url = zip_obj.get_temp_url(86400)
 
     finally:
-        # delete temp
-        shutil.rmtree(tempdir)
+        # mail zip url
+
+        # load in the templates
+        plaintext = get_template('zip_url.txt')
+        html = get_template('zip_url.html')
+
+        # setup the template context variables
+        d = Context({ 'zip_temp_url': zip_temp_url })
+
+        # build the email
+        email = AccountUser.objects.get(account_id=event.account_id).user.email
+        subject, from_email, to = 'Your Snapable album is ready for download', 'support@snapable.com', [email]
+        text_content = plaintext.render(d)
+        html_content = html.render(d)
+        msg = EmailMultiAlternatives(subject, text_content, from_email, to)
+        msg.attach_alternative(html_content, "text/html")
+        #if settings.DEBUG == False:
+        msg.send()
+
+        # delete local zip
+        os.remove(zip_path)
+
