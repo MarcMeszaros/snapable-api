6290b9affcf16935b6656175021344ebfc687dbc
JonathanSeguin <jonathan@snapable.com>
2014-01-10 22:16:58 -0500

    Added redis lock and removed all timestamp code (more or less reverting back to original code). Kept mail code.

--- worker/event.py
+++ worker/event.py
@@ -8,9 +8,6 @@ import shutil
 import zipfile
 import tempfile
 
-from datetime import datetime, timedelta
-from dateutil import parser
-
 # django/tastypie/libs
 import pyrax
 
@@ -28,6 +25,9 @@ from api.utils.loggers import Log
 @app.task
 def create_album_zip(event_id):
 
+    # add lock
+    app.backend.set('create_album_zip_'+event_id, 'RUNNING')
+
     event = Event.objects.get(pk=event_id)
 
     # establish a connection to the CDN
@@ -40,75 +40,46 @@ def create_album_zip(event_id):
         conn.make_container_public(cont.name)
         Log.i('created a new CDN container: ' + settings.RACKSPACE_CLOUDFILE_DOWNLOAD_CONTAINER_PREFIX + str(event.pk / settings.RACKSPACE_CLOUDFILE_EVENTS_PER_CONTAINER))
 
+    # (add try IO error block?)
+
     # create tempdir and get the photos
     tempdir = tempfile.mkdtemp(prefix='snap_api_event_{0}_'.format(event_id))
     photos = event.photo_set.all()
 
-    # Three scenarios are possible
-    # 1) zip already exists on CDN and is rather old -> recreate and reup
-    # 2) zip already exists on CDN and is rather new -> simply mail url for current zip
-    # 3) zip does not exist on CDN -> create zip and upload
+    # loop through all the photos, and save to disk on the worker server
+    for photo in photos:
+        photo.get_image().img.save('{0}/{1}.jpg'.format(tempdir, photo.pk))
 
-    try:
-        # check if zip already created on CDN
-        zip_obj = cont.get_object(event.title + ".zip")
+    # create and upload the zip file
+    zip_path = shutil.make_archive(tempfile.tempdir+"/"+event.title,'zip', tempdir)
+    zip_obj = cont.upload_file(zip_path)
 
-    except pyrax.exceptions.NoSuchObject as e:
-        # no zip for event on CDN yet -> upload to the CDN
+    # cleanup
+    os.remove(zip_path)
+    shutil.rmtree(tempdir)
 
-        # loop through all the photos, and save to disk on the worker server
-        for photo in photos:
-            photo.get_image().img.save('{0}/{1}.jpg'.format(tempdir, photo.pk))
+    # issue temp URL (24h = 86400 seconds)
+    zip_temp_url = zip_obj.get_temp_url(86400)
 
-        # create and upload the zip file
-        zip_path = shutil.make_archive(tempfile.tempdir+"/"+event.title,'zip', tempdir)
-        zip_obj = cont.upload_file(zip_path)
+    # mail zip url
 
-        # delete local zip
-        os.remove(zip_path)
+    # load in the templates
+    plaintext = get_template('zip_url.txt')
+    html = get_template('zip_url.html')
 
-    else:
-        # zip already created on CDN...
-        td = datetime.utcnow() - parser.parse(zip_obj.last_modified)
+    # setup the template context variables
+    d = Context({ 'zip_temp_url': zip_temp_url })
 
-        # zip was created a while ago (>15min) -> needs refresh
-        if td.seconds/60 > 15:
-             # loop through all the photos, and save to disk on the worker server
-            for photo in photos:
-                photo.get_image().img.save('{0}/{1}.jpg'.format(tempdir, photo.pk))
+    # build the email
+    email = AccountUser.objects.get(account_id=event.account_id).user.email
+    subject, from_email, to = 'Your Snapable album is ready for download', 'support@snapable.com', [email]
+    text_content = plaintext.render(d)
+    html_content = html.render(d)
+    msg = EmailMultiAlternatives(subject, text_content, from_email, to)
+    msg.attach_alternative(html_content, "text/html")
 
-            # create and upload the zip file
-            zip_path = shutil.make_archive(tempfile.tempdir+"/"+event.title,'zip', tempdir)
+    #if settings.DEBUG == False:
+    msg.send()
 
-            zip_obj.delete
-            zip_obj = cont.upload_file(zip_path)
-
-            # delete local zip
-            os.remove(zip_path)
-
-    finally:
-        # cleanup
-        shutil.rmtree(tempdir)
-
-        # issue temp URL (24h = 86400 seconds)
-        zip_temp_url = zip_obj.get_temp_url(86400)
-
-        # mail zip url
-
-        # load in the templates
-        plaintext = get_template('zip_url.txt')
-        html = get_template('zip_url.html')
-
-        # setup the template context variables
-        d = Context({ 'zip_temp_url': zip_temp_url })
-
-        # build the email
-        email = AccountUser.objects.get(account_id=event.account_id).user.email
-        subject, from_email, to = 'Your Snapable album is ready for download', 'support@snapable.com', [email]
-        text_content = plaintext.render(d)
-        html_content = html.render(d)
-        msg = EmailMultiAlternatives(subject, text_content, from_email, to)
-        msg.attach_alternative(html_content, "text/html")
-
-        #if settings.DEBUG == False:
-        msg.send()
+    # remove lock
+    app.backend.delete('create_album_zip_'+event_id)
