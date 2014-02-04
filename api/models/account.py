607270c18193a72322068c61a22b14cafd3ee686
Marc Meszaros <marc@snapable.com>
2014-02-04 00:57:37 -0500

    Add the API models to the admin

--- api/models/account.py
+++ api/models/account.py
@@ -1,5 +1,9 @@
+# django/tastypie/libs
+from django.contrib import admin
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class ApiAccount(models.Model):
 
     # required to make 'south' migrations work
@@ -10,10 +14,17 @@ class ApiAccount(models.Model):
     company = models.CharField(max_length=255, null=True, help_text='The name of the organization or company.')
     created = models.DateTimeField(auto_now_add=True, help_text='When the api account was created. (UTC)')
 
-    def __unicode__(self):
+    def __str__(self):
+        return '{0} - {1} ({2})'.format(self.pk, self.email, self.company)
+
+    def __repr__(self):
         return str({
             'email': self.email,
             'company': self.company,
             'created': self.created,
             'pk': self.pk,
         })
+
+class ApiAccountAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(ApiAccount, ApiAccountAdmin)
--- api/models/key.py
+++ api/models/key.py
@@ -1,15 +1,29 @@
+# python
 import hashlib
 import uuid
 
+# django/tastypie/libs
+from django.contrib import admin
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+# snapable
 from api.models import ApiAccount
 
+@python_2_unicode_compatible
 class ApiKey(models.Model):
     
     # required to make 'south' migrations work
     class Meta:
         app_label = 'api'
+
+    # the choices for the interval field
+    API_PARTNER_V1 = 'partner_v1'
+    API_PRIVATE_V1 = 'private_v1'
+    API_CHOICES = (
+        (API_PARTNER_V1, 'Partner (v1)'),
+        (API_PRIVATE_V1, 'Private (v1)'),
+    )
 
     # relations
     account = models.ForeignKey(ApiAccount)
@@ -17,11 +31,14 @@ class ApiKey(models.Model):
     # regular fields
     key = models.CharField(max_length=255, unique=True, db_index=True, help_text='The api key.')
     secret = models.CharField(max_length=255, help_text='The api key secret.')
-    version = models.CharField(max_length=25, help_text='The api version that the key has access to.')
+    version = models.CharField(max_length=25, choices=API_CHOICES, help_text='The api version that the key has access to.')
     created = models.DateTimeField(auto_now_add=True, help_text='When the api key was created. (UTC)')
     enabled = models.BooleanField(default=True, help_text='If the api key is enabled.')
 
-    def __unicode__(self):
+    def __str__(self):
+        return '{0} - ({1}) [{2}]'.format(self.pk, self.account.company, self.key)
+
+    def __repr__(self):
         return str({
             'api_account': self.api_account,
             'created': self.created,
@@ -51,3 +68,8 @@ class ApiKey(models.Model):
     @staticmethod
     def generate_secret():
         return hashlib.sha256(uuid.uuid4().hex).hexdigest()
+
+class ApiKeyAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(ApiKey, ApiKeyAdmin)
+
