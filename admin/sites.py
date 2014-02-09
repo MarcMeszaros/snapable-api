ceea13f71aa0b781789ccb306a61d9438afec96a
Marc Meszaros <marc@snapable.com>
2014-02-08 22:46:41 -0500

    Add initial code for the stats overview on the main admin page

--- admin/sites.py
+++ admin/sites.py
@@ -1,7 +1,17 @@
 import django.contrib.admin.sites
+from django.conf.urls import patterns, url
+
+import ajax.views
 
 class AdminSite(django.contrib.admin.sites.AdminSite):
-    pass
+
+    def get_urls(self):
+        base_patterns = super(AdminSite, self).get_urls()
+        new_patters = patterns('',
+            (r'^ajax/', ajax.views.index),
+        )
+
+        return new_patters + base_patterns
 
 # This global object represents the admin site, for the common case.
 site = AdminSite()
