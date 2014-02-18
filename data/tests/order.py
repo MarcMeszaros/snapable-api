50f4982f7e4d227c27331f1ffa3c8c0b6af6ce0e
Marc Meszaros <marc@snapable.com>
2014-02-17 23:18:11 -0500

    Add unit test for orders

---
+++ data/tests/order.py
@@ -0,0 +1,18 @@
+# django
+from django.test import TestCase
+
+# snapable
+from data.models import Order
+
+class OrderTestCase(TestCase):
+    fixtures = ['accounts_and_users.json', 'packages.json', 'orders.json']
+
+    def setUp(self):
+        """Setup the necessary objects for testing."""
+        pass
+
+    def test_calculate(self):
+        o = Order.objects.get(pk=1)
+        o.calculate()
+
+        self.assertEquals(o.amount, 7900)
