f49f73e3301fe353c1098b754f7dad25c599502b
Marc Meszaros <marc@snapable.com>
2014-04-28 12:40:26 -0400

    Add a new custom preprocessing function when building a list of objects to serialize

--- api/private_v1/resources/meta.py
+++ api/private_v1/resources/meta.py
@@ -1,5 +1,5 @@
 # django/tastypie/libs
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, Resource
 
 # snapable
 import api.auth
@@ -14,5 +14,35 @@ class BaseMeta(object):
     authorization = api.auth.ServerAuthorization()
     serializer = SnapSerializer(formats=['json', 'jpeg'])
 
-class BaseModelResource(ModelResource):
+class BaseResource(Resource):
+
+    # based on https://github.com/toastdriven/django-tastypie/blob/master/tastypie/resources.py#L1254
+    def build_get_list(self, request, resource, objects, **kwargs):
+        base_bundle = self.build_bundle(request=request)
+
+        # create the default resource_uri or override
+        resource_uri = self.get_resource_uri()
+        if 'resource_uri' in kwargs:
+            resource_uri = kwargs['resource_uri']
+
+        sorted_objects = self.apply_sorting(objects, options=request.GET)
+
+        paginator = self._meta.paginator_class(request.GET, sorted_objects, resource_uri=resource_uri, limit=self._meta.limit, max_limit=self._meta.max_limit, collection_name=self._meta.collection_name)
+        to_be_serialized = paginator.page()
+
+        # Dehydrate the bundles in preparation for serialization.
+        bundles = []
+        for obj in to_be_serialized[self._meta.collection_name]:
+            bundle = self.build_bundle(obj=obj, request=request)
+            bundles.append(resource.full_dehydrate(bundle, for_list=True))
+
+        to_be_serialized[self._meta.collection_name] = bundles
+        to_be_serialized = self.alter_list_data_to_serialize(request, to_be_serialized)
+        return to_be_serialized
+
+    # based on https://github.com/toastdriven/django-tastypie/blob/master/tastypie/resources.py#L1283
+    def build_get_detail(self, request, resource, object, **kwargs):
+        print('detail')
+
+class BaseModelResource(BaseResource, ModelResource):
     pass
