125f6ae8ecbf885b8d048518855683050fef09fa
Marc Meszaros <marc@snapable.com>
2015-02-13 23:01:33 -0800

    Remove all the vagrant stuff

--- salt/pillar/api.sls
+++
@@ -1,6 +0,0 @@
-snap_user:
-  user: vagrant
-
-mysql:
-  server:
-    bind-address: 0.0.0.0
--- vagrant/worker_bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "+--------------+"
-echo "| Setup Worker |"
-echo "+--------------+"
-echo ""
-rabbitmq-plugins enable rabbitmq_management
-service rabbitmq-server restart
