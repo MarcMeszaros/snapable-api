4d22c15cfcd233fe00a93afc2b6c81b3240a57e6
Marc Meszaros <marc@snapable.com>
2015-12-24 04:58:18 -0500

    Try and get config from etcd for redis

--- app/settings/redis.py
+++ app/settings/redis.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 import envitro
 import envitro.docker
+import etcd
+
+ETCD_HOST = envitro.str('ETCD_HOST', '172.17.42.1') # coreos default
+ETCD_KEY_REDIS = envitro.str('ETCD_KEY_REDIS', '/services/redis')
+
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+try:
+    client = etcd.Client(host=ETCD_HOST)
+    vals = client.read(ETCD_KEY_REDIS).value.rsplit(':', 1)
+    REDIS_HOST = vals[0]
+    REDIS_PORT = int(vals[1])
+except:
+    print('No ETCD connection')
+    REDIS_HOST = envitro.str('REDIS_HOST', envitro.docker.host('RD', '127.0.0.1'))
+    REDIS_PORT = envitro.int('REDIS_PORT', envitro.docker.port('RD', 6379))
 
 ##### Redis #####
-REDIS_HOST = envitro.str('REDIS_HOST', envitro.docker.host('RD', '127.0.0.1'))
-REDIS_PORT = envitro.int('REDIS_PORT', envitro.docker.port('RD', 6379))
 REDIS_DB = envitro.int('REDIS_DB', 0)
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,25 +1,2 @@
 # -*- coding: utf-8 -*-
-import re
-import sys
-
-# snapable
 from .loggers import Log
-
-class Dotable(dict):
-
-    __getattr__ = dict.__getitem__
-
-    def __init__(self, d):
-        if sys.version_info[:1] == (3,):
-            self.update(**dict((k, self.parse(v)) for k, v in d.items()))  # in py3 use .items
-        else:
-            self.update(**dict((k, self.parse(v)) for k, v in d.iteritems()))
-
-    @classmethod
-    def parse(cls, v):
-        if isinstance(v, dict):
-            return cls(v)
-        elif isinstance(v, list):
-            return [cls.parse(i) for i in v]
-        else:
-            return v
