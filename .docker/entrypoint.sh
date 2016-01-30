4cfcb504f7afb79063a47a2480dedd75c5cbdb81
Marc Meszaros <marc@snapable.com>
2016-01-30 01:16:57 -0800

    Update the entrypoint script

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -30,8 +30,28 @@ shell () {
   /src/bin/python manage.py shell
 }
 
+bash () {
+  /bin/bash
+}
+
 help () {
-  echo 'entrypoint.sh {api,worker,beat,shell} [migrate]'
+  echo "NAME:"
+  echo "\t entrypoint.sh - A wrapper script for starting the application."
+  echo ""
+  echo "USAGE:"
+  echo "\t entrypoint.sh {api,worker,beat,test,shell,bash} [migrate]"
+  echo ""
+  echo "POSITIONAL ARGUMENTS:"
+  echo "\t api \t\t Run the api process"
+  echo "\t worker \t Start the worker"
+  echo "\t beat \t\t Start a celery beat"
+  echo "\t test \t\t Run the test suite"
+  echo "\t shell \t\t Start a Django shell"
+  echo "\t bash \t\t Start a bash shell"
+  echo ""
+  echo "OPTIONAL POSITIONAL ARGUMENTS:"
+  echo "\t migrate \t Run Django migration before the positional argument"
+  echo ""
 }
 
 #run migrations if the extra param is appended
@@ -50,6 +70,8 @@ elif [ "$1" = "test" ]; then
   test
 elif [ "$1" = "shell" ]; then
   shell
+elif [ "$1" = "bash" ]; then
+  bash
 else
   help
 fi
