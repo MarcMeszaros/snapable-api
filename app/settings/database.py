c20a88ca93ac83c83f7a8b75f38f690354d46443
Marc Meszaros <marc@snapable.com>
2016-07-24 22:39:32 -0400

    Remove the default user name and password from the application

--- app/settings/database.py
+++ app/settings/database.py
@@ -9,9 +9,9 @@ pymysql.install_as_MySQLdb()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': envitro.str('DATABASE_NAME', 'snapabledb'),
-        'USER': envitro.str('DATABASE_USER', 'snapableusr'),
-        'PASSWORD': envitro.str('DATABASE_PASSWORD', 'snapable12345'),
+        'NAME': envitro.str('DATABASE_NAME'),
+        'USER': envitro.str('DATABASE_USER'),
+        'PASSWORD': envitro.str('DATABASE_PASSWORD'),
         'HOST': envitro.str('DATABASE_HOST', '127.0.0.1'),
         'PORT': envitro.int('DATABASE_PORT', 3306),
     }
