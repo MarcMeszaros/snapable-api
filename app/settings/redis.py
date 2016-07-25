b8b726c1ef497b2d3407743c8ab2d552c1d3de66
Marc Meszaros <marc@snapable.com>
2016-07-24 22:13:08 -0400

    Fix up the redis connection details

--- app/settings/redis.py
+++ app/settings/redis.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 import envitro
-import envitro.docker
 import etcd
 
 ETCD_HOST = envitro.str('ETCD_HOST', '172.17.42.1') # coreos default
 ETCD_PORT = envitro.int('ETCD_PORT', 2379)
 ETCD_KEY_REDIS = envitro.str('ETCD_KEY_REDIS', '/services/redis')
 
-REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 6379
-try:
-    client = etcd.Client(host=ETCD_HOST, port=ETCD_PORT)
-    vals = client.read(ETCD_KEY_REDIS).children.next().value.rsplit(':', 1)
-    REDIS_HOST = vals[0]
-    REDIS_PORT = int(vals[1])
-except:
-    print('No ETCD connection')
-    REDIS_HOST = envitro.str('REDIS_HOST', '127.0.0.1')
-    REDIS_PORT = envitro.int('REDIS_PORT', 6379)
+REDIS_HOST = envitro.str('REDIS_HOST', allow_none=True)
+REDIS_PORT = envitro.int('REDIS_PORT', allow_none=True)
+# if the redis host and port were not explicitly set, try and infer them from etcd
+if not (REDIS_HOST and REDIS_PORT):
+    try:
+        client = etcd.Client(host=ETCD_HOST, port=ETCD_PORT, read_timeout=5)
+        vals = client.read(ETCD_KEY_REDIS).children.next().value.rsplit(':', 1)
+        REDIS_HOST = vals[0]
+        REDIS_PORT = int(vals[1])
+    except:
+        print('No ETCD connection. Using localhost defaults.')
+        REDIS_HOST = '127.0.0.1'
+        REDIS_PORT = 6379
 
 ##### Redis #####
 REDIS_DB = envitro.int('REDIS_DB', 0)
